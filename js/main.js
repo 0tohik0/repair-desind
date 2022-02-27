4bfc96c21edbffe202f231be18b58164a104760a
0tohik0 <gryaznowa.katyu@yandex.ru>
2022-02-27 10:11:55 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector('.modal__close');
+    const closeBtn = document.querySelector ('.modal__close')
     const switchModal = () => {
         modal.classList.toggle('modal--visible');
     }
-    
-
     modalBtn.forEach(element => {
-      element.addEventListener('click', switchModal);
+        element.addEventListener('click', switchModal);
     });
-
-    closeBtn.addEventListener('click', switchModal);
-
- });
+    
+    closeBtn.addEventListener('click', switchModal)
+    document.addEventListener('click', (e) => {
+        if (e.target.classList === modal.classList) {
+            modal.classList.toggle('modal--visible');
+        };
+    });
+    
+    document.addEventListener('keydown', (e) => {
+        if(e.key == 'Escape') {
+            if (modal.classList.contains('modal--visible')) {
+                modal.classList.toggle('modal--visible');
+            };
+        };
+    });
+})
